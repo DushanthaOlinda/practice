088c398f5fe5649a7af0d98de6bd51e99ac4fa07
JanodUmayanga <2020cs189@stu.ucsc.cmb.ac.lk>
2021-09-28 03:23:05 +0530

    .

--- admin.php
+++ admin.php
@@ -90,6 +90,9 @@ a:active {
     <form action="./Insert/insertClient_admin.php" method="POST">
         <input type="submit" id="btn" value="Insert Client" name="Insert_client">
     </form>
+    <form action="./Insert/insertPropertyRequirenment.php" method="POST">
+        <input type="submit" id="btn" value="Insert Property Requirement" name="Insert_PR">
+    </form>
     <form action="./Insert/insertVisit_admin.php" method="POST">
     <input type="submit" id="btn" value="Insert Visit" name="Insert_visit">
     </form>
